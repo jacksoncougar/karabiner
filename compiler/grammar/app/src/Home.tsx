09642ee16361a445cf26652e6184f873b37d8e88
Jack <jacksoncougar@gmail.com>
2020-11-15 12:17:00 -0700

    feat: give feedback for compiler errors

--- compiler/grammar/app/src/Home.tsx
+++ compiler/grammar/app/src/Home.tsx
@@ -7,11 +7,12 @@ import { CommonTokenStream } from "antlr4ts/CommonTokenStream";
 import { KLLLexer } from "./antlr/KLLLexer";
 import { KLLParser } from "./antlr/KLLParser";
 import { KarabinerCompiler } from "./Compiler";
-import React from "react";
+import React, { useRef, useState } from "react";
+import { ANTLRErrorListener } from "antlr4ts/ANTLRErrorListener";
 
-class Home extends React.Component {
-  render() {
-    const input = `
+function Home(props) {
+  let textareaRef = useRef(null);
+  let [input, setInput] = useState(`
     caps_lock toggles layer1
     up_arrow is nothing
     left_arrow is nothing
@@ -54,26 +55,99 @@ class Home extends React.Component {
       u is 8
       i is 9
     done
-    `;
-    const chars = CharStreams.fromString(input);
-    const lexer = new KLLLexer(chars);
-    const tokens = new CommonTokenStream(lexer);
-    const parser = new KLLParser(tokens);
-    const visitor = new myKLLVisitor();
-    parser.buildParseTree = true;
+    `);
+  const chars = CharStreams.fromString(input);
+  let output = "";
+  let statusGood = true;
 
-    const tree = parser.config();
-    const table = visitor.visit(tree);
-    const layers = new LayerSynthesisVisitor(table).visit(tree);
-    const output = new KarabinerCompiler().compile(layers);
+  let listener: ANTLRErrorListener<string> = {
+    syntaxError: (r, o, l, c, msg, e): void => {
+      statusGood = false;
+    },
+  };
+  const lexer = new KLLLexer(chars);
+  const tokens = new CommonTokenStream(lexer);
+  const parser = new KLLParser(tokens);
+  const visitor = new myKLLVisitor();
+  parser.addErrorListener(listener);
+  parser.buildParseTree = true;
 
-    return (
-      <div className="compiler">
-        <pre>{input}</pre>
+  const tree = parser.config();
+  const table = visitor.visit(tree);
+  const layers = new LayerSynthesisVisitor(table).visit(tree);
+  output = new KarabinerCompiler().compile(layers);
+
+
+  let onKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Tab" && !e.shiftKey) {
+      e.preventDefault();
+      if (!textareaRef.current) return;
+      const value = textareaRef.current!.value;
+      const selectionStart = textareaRef.current!.selectionStart;
+      const selectionEnd = textareaRef.current!.selectionEnd;
+      textareaRef.current!.value =
+        value.substring(0, selectionStart) +
+        "  " +
+        value.substring(selectionEnd);
+      textareaRef.current!.selectionStart =
+        selectionEnd + 2 - (selectionEnd - selectionStart);
+      textareaRef.current!.selectionEnd =
+        selectionEnd + 2 - (selectionEnd - selectionStart);
+    }
+    if (e.key === "Tab" && e.shiftKey) {
+      e.preventDefault();
+      const value = textareaRef.current!.value;
+      const selectionStart = textareaRef.current!.selectionStart;
+      const selectionEnd = textareaRef.current!.selectionEnd;
+
+      const beforeStart = value
+        .substring(0, selectionStart)
+        .split("")
+        .reverse()
+        .join("");
+      const indexOfTab = beforeStart.indexOf("  ");
+      const indexOfNewline = beforeStart.indexOf("\n");
+
+      if (indexOfTab !== -1 && indexOfTab < indexOfNewline) {
+        textareaRef.current!.value =
+          beforeStart
+            .substring(indexOfTab + 2)
+            .split("")
+            .reverse()
+            .join("") +
+          beforeStart.substring(0, indexOfTab).split("").reverse().join("") +
+          value.substring(selectionEnd);
+
+        textareaRef.current!.selectionStart = selectionStart - 2;
+        textareaRef.current!.selectionEnd = selectionEnd - 2;
+      }
+    }
+  };
+
+  return (
+    <div className="compiler">
+      <div>
+        <textarea
+          ref={textareaRef}
+          onKeyDown={(e) => onKeyDown(e)}
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+      </div>
+      <div className={statusGood ? "good" : "bad"}>
+        <button
+          onClick={(e) => {
+            navigator.clipboard.writeText(output);
+          }}
+        >
+          Copy to clipboard
+        </button>
         <pre>{output}</pre>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Home;
